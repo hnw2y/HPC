08a70b579de5b8c973836dd5f46ddd75cd94a54e
hnw2y <hannah.n.williams.2000@gmail.com>
2021-04-27 02:45:41 +0000

    put ssh at end of script

--- start_services.sh
+++ start_services.sh
@@ -2,9 +2,9 @@
 cp /etc/munge/munge.key.tmp/MUNGE_KEY /etc/munge/munge.key
 chown munge:munge /etc/munge/munge.key
 chmod 0700 /etc/munge/munge.key
-service ssh start
 service rpcbind start
 service nfs-common start
 service munge start
 service slurmd start
+service ssh start
 exec /usr/sbin/sshd -D
