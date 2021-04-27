332c152cdfcac655057a43621893798d90f1a242
hnw2y <hannah.n.williams.2000@gmail.com>
2021-04-27 03:35:49 +0000

    reverted back

--- start_services.sh
+++ start_services.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/sh
 cp /etc/munge/munge.key.tmp/MUNGE_KEY /etc/munge/munge.key
 chown munge:munge /etc/munge/munge.key
 chmod 0700 /etc/munge/munge.key
@@ -7,4 +7,4 @@ service nfs-common start
 service munge start
 service slurmd start
 service ssh start
-exec /usr/sbin/sshd -D
+exec su - jovyan -c "sleep infinity"
